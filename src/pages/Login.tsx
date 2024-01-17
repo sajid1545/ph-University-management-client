853f92a31d81d2f7c3b80c96c41cec82d8a97ce2
Sajjad Abdullah <clashking1545@gmail.com>
2024-01-17 22:10:24 +0600

    module 6-8 completed

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,49 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/Auth/authApi";
+import { setUser } from "../redux/features/Auth/authSlice";
+import { useAppDispatch } from "../redux/hooks";
+import { verifyToken } from "../utils/verifyToken";
+
 const Login = () => {
-	return <div>Login</div>;
+	const dispatch = useAppDispatch();
+
+	const { register, handleSubmit } = useForm({
+		defaultValues: {
+			id: "A-0001",
+			password: "admin12345",
+		},
+	});
+
+	const [login, { error }] = useLoginMutation();
+
+	const onSubmit = async (data) => {
+		const userInfo = {
+			id: data.id,
+			password: data.password,
+		};
+
+		const res = await login(userInfo).unwrap();
+		const user = verifyToken(res.data.accessToken);
+
+		dispatch(setUser({ user: user, token: res.data.accessToken }));
+	};
+
+	return (
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<div>
+				<label htmlFor="id">Id</label>
+				<input type="text" id="id" {...register("id")} />
+			</div>
+			<div>
+				<label htmlFor="password">Password</label>
+				<input type="text" id="password" {...register("password")} />
+			</div>
+			<Button type="primary" htmlType="submit">
+				Login
+			</Button>
+		</form>
+	);
 };
 
 export default Login;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { baseApi } from "./api/baseApi";
+import authReducer from "./features/Auth/authSlice";
+
+const persistConfig = {
+	key: "auth",
+	storage,
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
 
 export const store = configureStore({
-	reducer: {},
+	reducer: {
+		[baseApi.reducerPath]: baseApi.reducer,
+		auth: persistedAuthReducer,
+	},
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
